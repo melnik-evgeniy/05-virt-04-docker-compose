24f133d022de0fd76cd5a44fcf1093f17d20ad5c
melnik-evgeniy <melnik1988@mail.ru>
2022-04-20 21:36:49 +0300

    master

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g07h2e1uu05chge9uc"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gpoam8bmqo3hh7c2h1"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd83ksv4j37g849ha33g"
+}
